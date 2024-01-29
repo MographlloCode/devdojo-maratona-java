043856557f64de7d75ae95fe392d1d7c23d6f8e9
MographlloCode <gmellocontact@gmail.com>
2024-01-29 18:17:54 -0300

    Exercício 01 - Aula 02

--- src/dev/mographllo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/dev/mographllo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,5 +1,20 @@
 package dev.mographllo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
-    
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int fatherAge = 43;
+        System.out.println("A idade do pai é " + fatherAge + " anos");
+
+        long largeNumber = 1000000;
+        double salaryDouble = 6143.13;
+        float salaryFloat = 6143.0F;
+        byte ageBYte = 10;
+        short ageShort = 10;
+        boolean verdade = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        String name = "Strings sempre em aspas duplas";
+    }
 }
---
+++ src/dev/mographllo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,26 @@
+package dev.mographllo.maratonajava.introducao;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Aula03TiposPrimitivosExercicio {
+    /*
+    Prática
+
+    Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+    Eu <nome>, morando no endereço <endereco>,
+    confirmo que recebi o salário de <salario>, na data <data>
+    */
+    public static void main(String[] args) {
+        String nome = "Gustavo";
+        String endereco = "Rua das Laranjeiras 53";
+        double salario = 6500.32;
+        LocalDate data = LocalDate.now();
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataFormatada = data.format(formatador);
+        System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de R$"+salario+", na data "+dataFormatada);
+    }
+
+
+}
