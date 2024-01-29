45bab7561848a0becf93064e30007dfbac72b03d
MographlloCode <gmellocontact@gmail.com>
2024-01-29 18:23:09 -0300

    fix - espaçamento exercício 01

--- src/dev/mographllo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ src/dev/mographllo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -16,9 +16,11 @@ public class Aula03TiposPrimitivosExercicio {
         String nome = "Gustavo";
         String endereco = "Rua das Laranjeiras 53";
         double salario = 6500.32;
+
         LocalDate data = LocalDate.now();
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String dataFormatada = data.format(formatador);
+
         System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de R$"+salario+", na data "+dataFormatada);
     }
 
